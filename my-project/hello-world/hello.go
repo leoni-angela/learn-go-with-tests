6ad29ed1883fad089f4cababa7e8b031d53b3a9f
leoni-angela <200555998+leoni-angela@users.noreply.github.com>
2025-09-02 15:06:53 +1000

    completed hello-world module

--- my-project/hello-world/hello.go
+++ my-project/hello-world/hello.go
@@ -3,15 +3,37 @@ package main
 import "fmt"
 
 // try to make constants to capure meaning of values (can aid performance too)
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+	
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+// starts uppercase for public functions to be exported
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+// starts lowercase for private functions to not be exported
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return // no need to add prefix because of our func siganture preset that
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("", ""))
 }
