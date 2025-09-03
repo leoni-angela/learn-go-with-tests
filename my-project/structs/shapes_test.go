5e3fd4e5350a57ac1634b78c909e6aa990ab975a
leoni-angela <200555998+leoni-angela@users.noreply.github.com>
2025-09-03 14:21:52 +1000

    completed structs module

--- my-project/structs/shapes_test.go
+++ my-project/structs/shapes_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
-	want := 40.0
+	hasArea := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	if got != hasArea {
+		t.Errorf("got %.2f hasArea %.2f", got, hasArea)
 	}
 
 }
@@ -16,33 +16,26 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name string
 		shape Shape
-		want float64
+		hasArea float64
 	}{
-		
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g, got, want")
-			// As you can see, the f has been replaced by g. 
-			// Use of g will print a more precise decimal number in the error message
-		}
+	for _, tt := range areaTests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%v got %g want area %g", tt.shape, got, tt.hasArea)
+				// As you can see, the f has been replaced by g.
+				// Use of g will print a more precise decimal number in the error message
+			}
+		})
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		want := 72.0
-		checkArea(t, rectangle, want)
-		
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
 
 }
